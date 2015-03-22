f87d426b8d2d21a107327ff3056f66204cdb0c12
devinacker <d@revenant1.net>
2015-03-22 18:30:50 -0400

    v1.13

--- src/version.h
+++ src/version.h
@@ -8,11 +8,11 @@
 
 #define INFO_TITLE  "Kirby's Dream Course Editor"
 #define INFO_MYNAME "Devin Acker (Revenant)\0"
-#define INFO_LEGAL  "Copyright 2013 by Revenant\0"
+#define INFO_LEGAL  "Copyright 2013-2015 by Revenant\0"
 
 #define INFO_DESC   "Kirby's Dream Course editor\0"
-#define INFO_VERS   "1.12\0"
-#define INFO_VNUM   1, 12, 0, 0
+#define INFO_VERS   "1.13\0"
+#define INFO_VNUM   1, 13, 0, 0
 #define INFO_NAME   "KDCEditor\0"
 #define INFO_FILE   "KDCEditor.exe\0"
 
